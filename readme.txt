2a2e14c7a7e13acf69fca4291504f948ce170fcb
naokiasada <siotan0922@yahoo.co.jp>
2026-03-08 22:53:33 +0900

    設定

--- readme.txt
+++ readme.txt
@@ -1,1 +1,3 @@
+cd C:\ws\java\p2p
+
 & "C:\Program Files\nodejs\node.exe" .\node_modules\browser-sync\dist\bin.js start --proxy localhost:8080 --files "src/main/resources/static/css/**/*.css,src/main/resources/templates/**/*.html" --open false
---
+++ src/main/java/com/example/p2p/Controller/SettingController.java
@@ -0,0 +1,16 @@
+package com.example.p2p.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/setting")
+public class SettingController {
+
+    @GetMapping
+    public String showSetting() {
+        return "setting";
+    }
+
+}
