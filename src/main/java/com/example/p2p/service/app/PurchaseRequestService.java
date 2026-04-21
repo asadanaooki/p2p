ad85c1460c6eff1cc3b50201907ffa46b7fa8de9
naokiasada <siotan0922@yahoo.co.jp>
2026-04-21 15:03:25 +0900

    一時コミット

---
+++ src/main/java/com/example/p2p/service/app/PurchaseRequestService.java
@@ -0,0 +1,6 @@
+package com.example.p2p.service.app;
+
+public class PurchaseRequestService {
+    <table tableName="user_invitation_token"></table>
+}
+
---
+++ src/main/resources/db/migration/V17__create_purchase_request.sql
@@ -0,0 +1,17 @@
+create table purchase_request (
+    pr_id char(36) primary key default gen_random_uuid () ::text,
+    display_number SERIAL not null unique,
+    supplier_id char(36) not null,
+    requester_user_id char(36) not null,
+    total_amount integer not null check(total_amount >= 0),
+    status varchar(30) not null check(status in ('PENDING', 'APPROVED', 'REJECTED', 'CANCELLED', 'COMPLETED')),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    foreign key (supplier_id) references supplier(supplier_id),
+    foreign key (requester_user_id) references users(user_id)
+);
+
+create trigger update_purchase_request_modtime
+before update on purchase_request
+for each row
+execute procedure update_timestamp();
